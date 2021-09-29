a8bc4a453a66fc2765e74f6b9632919a86fa0fcd
egools <ergools59@gmail.com>
2021-09-29 00:18:13 -0400

    Inject yahoo service

--- Program.cs
+++ Program.cs
@@ -1,19 +1,30 @@
 using Microsoft.Azure.Functions.Worker.Configuration;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
+using YahooFantasyService;
 
 namespace GoolsDev.Functions.FantasyFootball
 {
     public class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
             var host = new HostBuilder()
                 .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(s =>
+                {
+                    s.AddOptions<YahooServiceSettings>()
+                    .Configure<IConfiguration>((settings, configuration) =>
+                    {
+                        configuration.GetSection("YahooServiceSettings").Bind(settings);
+                    });
+                    s.AddSingleton<IYahooService, YahooService>();
+                })
                 .Build();
 
-            host.Run();
+            await host.RunAsync();
         }
     }
 }
